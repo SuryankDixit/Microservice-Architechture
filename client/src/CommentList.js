a90f0ce651b1ae3577c2bf4efd1e6a8b978c5479
SuryankDixit <suryankdixit@gmail.com>
2021-01-05 00:06:41 +0530

    Commets Filtering Updated frontend.

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 export default ({ comments }) => {
 
-  const renderedComments = comments.map(comment => {
-    return <li key={comment.id}>{comment.content}</li>;
-  });
+    const renderedComments = comments.map(comment => {
 
-  return <ul>{renderedComments}</ul>;
+        let content;
+        if (comment.status === 'approved') {
+            content = comment.content;
+        }
+
+        if (comment.status === 'pending') {
+            content = 'This content is awaiting moderation';
+        }
+
+        if (comment.status === 'rejected') {
+            content = 'Marked as Spam';
+        }
+
+        return <li key = { comment.id } > { content } < /li>;
+    });
+
+    return <ul > { renderedComments } < /ul>;
 };
