2836bd96e168f75244d63001a32d77191033a558
SuryankDixit <suryankdixit@gmail.com>
2021-01-08 21:27:04 +0530

    Dockerized Service.

--- query/index.js
+++ query/index.js
@@ -51,9 +51,9 @@ app.post('/events', (req, res) => {
 app.listen(4002, async() => {
     console.log('Listening on 4002');
 
-    const res = await axios.get("http://localhost:4005/events");
-    for (let event of res.data) {
-        console.log('Procesing Event:', event.type);
-        handleEvent(event.type, event.data);
-    }
+    // const res = await axios.get("http://localhost:4005/events");
+    // for (let event of res.data) {
+    //     console.log('Procesing Event:', event.type);
+    //     handleEvent(event.type, event.data);
+    // }
 });
