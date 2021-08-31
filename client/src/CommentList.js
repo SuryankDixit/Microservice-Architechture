c3a250ea34461ae8232b632802a818af8fabb35a
SuryankDixit <suryankdixit@gmail.com>
2021-08-31 13:08:58 +0530

    Dumpt comment List component

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -17,8 +17,35 @@ export default ({ comments }) => {
             content = 'Marked as Spam';
         }
 
-        return <li key = { comment.id } > { content } < /li>;
+        return <li key={comment.id}> {content} </li>;
     });
 
-    return <ul > { renderedComments } < /ul>;
+    return <ul> {renderedComments} </ul>;
 };
+
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+
+// const CommentList = ({ postId }) => {
+//     const [comments, setComments] = useState([]);
+
+//     const fetchData = async () => {
+//         const res = await axios.get(
+//             `http://localhost:4001/posts/${postId}/comments`
+//         );
+
+//         setComments(res.data);
+//     };
+
+//     useEffect(() => {
+//         fetchData();
+//     }, []);
+
+//     const renderedComments = comments.map((comment) => {
+//         return <li key={comment.id}>{comment.content}</li>;
+//     });
+
+//     return <ul>{renderedComments}</ul>;
+// };
+
+// export default CommentList;
