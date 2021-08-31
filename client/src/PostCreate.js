6d4bd349ef7e44ad08c73baac7b6fd503b0cc5a0
SuryankDixit <suryankdixit@gmail.com>
2021-08-31 13:09:20 +0530

    Dump all posts components

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export default () => {
   const [title, setTitle] = useState('');
 
-  const onSubmit = async event => {
+  const onSubmit = async (event) => {
     event.preventDefault();
 
     await axios.post('http://localhost:4000/posts', {
@@ -21,7 +21,7 @@ export default () => {
           <label>Title</label>
           <input
             value={title}
-            onChange={e => setTitle(e.target.value)}
+            onChange={(e) => setTitle(e.target.value)}
             className="form-control"
           />
         </div>
