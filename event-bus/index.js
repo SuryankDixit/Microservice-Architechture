b0df1f0db99e3516ab1e16d2b9fada549650c00a
SuryankDixit <suryankdixit@gmail.com>
2021-08-31 13:10:23 +0530

    Event Bus service

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const axios = require('axios');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-
 const app = express();
-app.use(bodyParser.json());
 
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
+// storing all the events that come to event bus for future references
 const events = [];
 
 app.post("/events", (req, res) => {
-    const event = req.body;
+    const event = req.body; // whatever comes in our req body , take that in event
 
     events.push(event);
 
     console.log(event);
     // the downside is clearly visible here ,we are assuming that every post request will succeed eventually;
-    axios.post("http://localhost:4000/events", event);
-    axios.post("http://localhost:4001/events", event);
-    axios.post("http://localhost:4002/events", event);
-    axios.post("http://localhost:4003/events", event);
+    axios.post("http://localhost:4000/events", event); // post
+    axios.post("http://localhost:4001/events", event); // comment
+    axios.post("http://localhost:4002/events", event); // query service
+    axios.post("http://localhost:4003/events", event); // moderation service
 
     res.send({ status: "OK" });
 });
